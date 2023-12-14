c1099ffd6f32a156a27a90dfa2fe53dfea04e5c4
Amanda Mkhize <amandamkhize@yahoo.com>
2023-12-13 19:43:03 -0800

    main.c

--- main.c
+++ main.c
@@ -1,15 +1,4 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-#define BUF_SIZE 256
-
-void prompt_and_read_input(char **user_input);
-void handle_command(char **cmd, char **user_input,
-		char **argv, int *count, int *stat);
 
 /**
 * alt_main - Entry point to the program
@@ -21,70 +10,52 @@ int alt_main(__attribute__((unused)) int argc, char **argv)
 {
 char *user_input, **cmd, **commands;
 int count = 0, i, condition = 1, stat = 0;
-signal(SIGINT, signal_to_handle);
 
+if (argv[1] != NULL)
+read_file(argv[1], argv);
+signal(SIGINT, signal_to_handle);
 while (condition)
 {
 count++;
-prompt_and_read_input(&user_input);
+if (isatty(STDIN_FILENO))
+prompt();
+user_input = _getline();
 if (user_input[0] == '\0')
 continue;
-
 history(user_input);
 commands = separator(user_input);
-
 for (i = 0; commands[i] != NULL; i++)
 {
 cmd = parse_cmd(commands[i]);
-handle_command(cmd, &user_input, argv, &count, &stat);
+if (_strcmp(cmd[0], "exit") == 0)
+{
+free(commands);
+exit_bul(cmd, user_input, argv, count, stat);
 }
-
+else if (check_builtin(cmd) == 0)
+{
+stat = handle_builtin(cmd, stat);
+free(cmd);
+continue;
+}
+else
+stat = check_cmd(cmd, user_input, count, argv);
+free(cmd);
+}
 free(user_input);
 free(commands);
 wait(&stat);
 }
-
 return (stat);
 }
 
-/**
-* prompt_and_read_input - Prompt the user and read input
-* @user_input: Pointer to store user input
-*/
-void prompt_and_read_input(char **user_input)
-{
-if (isatty(STDIN_FILENO))
-prompt();
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-*user_input = _getline();
-}
-
-/**
-* handle_command - Handle the command
-* @cmd: Parsed command
-* @user_input: User input
-* @argv: Argument vector
-* @count: Pointer to count
-* @stat: Pointer to status
-*/
-void handle_command(char **cmd, char **user_input,
-		char **argv, int *count, int *stat)
-{
-if (_strcmp(cmd[0], "exit") == 0)
-{
-free(*cmd);
-exit_bul(cmd, *user_input, argv, *count, *stat);
-}
-else if (check_builtin(cmd) == 0)
-{
-*stat = handle_builtin(cmd, *stat);
-free(*cmd);
-}
-else
-*stat = check_cmd(cmd, *user_input, *count, argv);
-
-free(*cmd);
-}
+#define BUF_SIZE 256
 
 /**
 * read_command - Read a command from the user
@@ -92,11 +63,13 @@ free(*cmd);
 * @prompt: The prompt to display to the user
 * Return: Returns 1 if successful, 0 otherwise
 */
+
 int read_command(char *buffer, const char *prompt)
 {
 printf("%s", prompt);
 
 return (fgets(buffer, BUF_SIZE, stdin) != NULL);
+
 }
 
 /**
@@ -104,6 +77,7 @@ return (fgets(buffer, BUF_SIZE, stdin) != NULL);
 * @buffer: The buffer to check for end-of-file
 * Return: Returns 1 if end-of-file is reached, 0 otherwise
 */
+
 int handle_eof(__attribute__((unused)) char *buffer)
 {
 if (feof(stdin))
@@ -119,6 +93,7 @@ return (0);
 * @buffer: The command to process
 * Return: Returns 0 if successful, 1 otherwise
 */
+
 int process_command(char *buffer)
 {
 pid_t pid;
@@ -129,7 +104,6 @@ return (0);
 }
 
 pid = fork();
-
 if (strchr(buffer, ' ') != NULL)
 {
 printf("Error: Command should be a single word\n");
@@ -163,12 +137,13 @@ printf("Error executing command\n");
 return (0);
 }
 /**
-* main - Entry point to the shell program
-* Return: Exit status
-*/
+ * main - Entry point of the shell program
+ * Return: 0 on success, 1 on failure
+ */
 int main(void)
 {
 char buffer[BUF_SIZE];
+
 const char prompt[] = "#cisfun$ ";
 
 while (1)
